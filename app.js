f990dfa0745c83a9829073d4a686364169794b45
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-22 10:37:59 -0500

    Completed bonus objective #1

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ class Dice {
 }
 
 $('#generateDie').click(function () {
+    $('#rollAll').attr('hidden', false);
+    $('#sumDice').attr('hidden', false);
     dice.push(new Dice());
     $('#diceBox').append(`<div class='die' id='die${divIndex}'>${dice[divIndex].value}</div>`);
     divIndex++;
@@ -23,4 +25,12 @@ $('#rollAll').click(function () {
         dice[i].roll();
         $(`#die${i}`).text(dice[i].value);
     }
+});
+
+$('#sumDice').click(function () {
+    let sum = 0;
+    dice.forEach(die => {
+        sum += die.value;
+    });
+    alert(`The sum of all the dice is ${sum}`);
 });
